b9264af26dc27054b5e9d6b9b378903154101828
kevin <kpogrund@gmail.com>
2021-10-31 01:12:17 +0200

    normalize fix

--- dataloader.py
+++ dataloader.py
@@ -125,7 +125,7 @@ class DataLoadPreprocess(Dataset):
                 depth_gt = depth_gt / 1000.0
             else:
                 depth_gt = depth_gt / 256.0
-            depth_gt = (depth_gt -depth_gt.max())/(depth_gt.max()-depth_gt.min())  # normalize to 0-1
+            depth_gt = (depth_gt - depth_gt.min()) / (depth_gt.max() - depth_gt.min())  # normalize to 0-1
 
             print(f"depth gt max {depth_gt.max()}")
             print(f"image max {image.max()}")
@@ -161,7 +161,7 @@ class DataLoadPreprocess(Dataset):
                     else:
                         depth_gt = depth_gt / 256.0
 
-                depth_gt = (depth_gt - depth_gt.max()) / (depth_gt.max() - depth_gt.min())  # normalize to 0-1
+                depth_gt = (depth_gt - depth_gt.min()) / (depth_gt.max() - depth_gt.min())  # normalize to 0-1
             if self.args.do_kb_crop is True:
                 height = image.shape[0]
                 width = image.shape[1]
